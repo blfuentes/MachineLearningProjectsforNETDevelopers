45b63dbb65ff259ed5c6be6bacea384c6c2bf874
bernardo <epiphonetodie@gmail.com>
2018-10-31 13:35:19 +0100

    Page 92
    NaiveBayes

---
+++ 02_HamOrSpam/Script.fsx
@@ -0,0 +1,42 @@
+﻿open System.IO
+
+type DocType =
+    | Ham
+    | Spam
+
+let parseDocType (label:string) =
+    match label with
+    | "ham"     -> Ham
+    | "spam"    -> Spam
+    | _         -> failwith "Unknown label"
+
+let parseLine (line:string) =
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\Data\" + fileName
+
+let dataset =
+    File.ReadAllLines path
+    |> Array.map parseLine
+
+let spamWithFREE =
+    dataset
+    |> Array.filter (fun (docType, _) -> docType = Spam)
+    |> Array.filter (fun (_, sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let hamWithFREE =
+    dataset
+    |> Array.filter (fun (docType, _) -> docType = Ham)
+    |> Array.filter (fun (_, sms) -> sms.Contains("FREE"))
+    |> Array.length
+
+let primitiveClassifier (sms:string) =
+    if (sms.Contains "FREE")
+    then Spam
+    else Ham
+
